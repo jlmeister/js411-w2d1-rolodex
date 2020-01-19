06ca3ba8a275d5b84ed253c706c91c66d741c09b
jlmeister <iamphil4483@yahoo.com>
2020-01-18 18:02:08 -0600

    fetch request for random user data, display basic info to the screen with a button to conditionally render the rest of the user information. final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>
+        Some Random Users 😛
+      </h1>
+    </header>
+  )
+}
+
+const List = ({ list }) => {
+  return (
+    <div className="list">
+      {list.map((user) => (
+        <UserCard key={user.email} {...user} />
+      ))}
     </div>
-  );
+  )
+}
+
+class UserCard extends Component {
+  state = {
+    resultsHidden: true
+  }
+
+  handleClick = () => {
+    this.setState({
+      resultsHidden: !this.state.resultsHidden
+    });
+  }
+
+  render() {
+    console.log('user card props: ', this.props);
+    return (
+      <div className="card">
+        <div className="card-header">
+          <img src={this.props.avatar} />
+          <p>{this.props.name}</p>
+          <button onClick={this.handleClick}>{this.state.resultsHidden ? 'show details' : 'hide details'}</button>
+        </div>
+        {!this.state.resultsHidden && (
+          <>
+            <p>email: {this.props.email}</p>
+            <p>username: {this.props.username}</p>
+            <p>age: {this.props.age}</p>
+          </>  
+        )}
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    users: [],
+    isLoaded: false
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api?results=25')
+      .then(response => response.json())
+      .then(parsedJSON => parsedJSON.results.map(user => (
+        {
+          name: `${user.name.first} ${user.name.last}`,
+          email: user.email,
+          username: user.login.username,
+          avatar: user.picture.thumbnail,
+          age: user.dob.age
+        }
+      )))
+      .then(users => this.setState({
+        users: users,
+        isLoaded: true
+      }))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {this.state.isLoaded ? <List list={this.state.users}/> : <p>loading...</p>}
+      </div>
+    )
+  }
 }
 
 export default App;
